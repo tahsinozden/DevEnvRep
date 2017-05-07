0f40c0195d3075bd52da81bbccd5c3c2cc844b27
tahsin <tahsinozden@gmail.com>
2017-05-07 13:29:45 +0200

    message service added.

---
+++ webapp/src/main/java/com/webapp/services/MessageService.java
@@ -0,0 +1,13 @@
+package com.webapp.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by tahsin on 2017-05-07.
+ */
+@Service
+public class MessageService {
+    public void sendMessage(String from, String to) {
+        // TODO: implement here
+    }
+}
